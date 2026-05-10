032fd4778df08bd32d8b3dcfe78853388bb09b17
ABDUL RAFAY <154729562+aabdulrafay@users.noreply.github.com>
2026-05-10 08:20:18 +0500

    Initial commit

---
+++ Assets/Scripts/Zombie/ZombieMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMove : MonoBehaviour
+{
+
+    public float speed = 20f;
+	// Use this for initialization
+    [HideInInspector]
+    public int row;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Translate(-speed*Time.deltaTime,0,0);
+	}
+}
